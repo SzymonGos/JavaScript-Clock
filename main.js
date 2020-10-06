028e6fcf2ac7afd86e42fe16d33d37bb222c697d
Szymon Gos <37188746+SzymonGos@users.noreply.github.com>
2020-10-06 18:32:18 +0100

    -- simplify arrays of days and months

--- main.js
+++ main.js
@@ -18,33 +18,11 @@ function showTime(){
 function showDate(){
     const date = new Date();
     let dayOfTheMonth = date.getDate();  
-    
-    
-    const weekday = new Array();
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    
-    const months = new Array();
-    months[0] = "January";
-    months[1] = "February";
-    months[2] = "March";
-    months[3] = "April";
-    months[4] = "May";
-    months[5] = "June";
-    months[6] = "July";
-    months[7] = "August";
-    months[8] = "September";
-    months[9] = "October";
-    months[10] = "November";
-    months[11] = "December";
+   
+    const weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
+    const months = ['January', 'February','March','April','May','June','July','August','September','October','November','December'];
     
-
     const day = weekday[date.getDay()];
     const month = months[date.getMonth()];
 
